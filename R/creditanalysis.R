6d3b3a71740eed7b80905cce6aa0dbc2355bf253
Ryan <kuhnrl30@gmail.com>
2017-04-19 23:22:26 -0400

    update project site structure

---
+++ R/creditanalysis.R
@@ -0,0 +1,8 @@
+#' Coursework for Audit Analytics II
+#'
+#' Documenting the coursework and final project for the Audit Analytics II
+#' course at Rutgers University. This course was taken as part of the
+#' Masters in Financial Accountancy program.
+#' @docType package
+#' @name CreditAnalysis
+NULL
